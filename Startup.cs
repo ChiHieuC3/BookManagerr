6547f0189a8029835b11a7f35d0d7736cabfd297
ChiHieuC3 <77269574+ChiHieuC3@users.noreply.github.com>
2021-06-26 23:47:34 +0700

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookManagerr.Startup))]
+namespace BookManagerr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
